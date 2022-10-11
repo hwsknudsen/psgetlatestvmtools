9ca996ad450b256688941e3277b585342d68f3dd
hwsknudsen <hwsknudsen@users.noreply.github.com>
2022-10-10 22:16:09 -0300

    Add files via upload

---
+++ get.ps1
@@ -0,0 +1,11 @@
+﻿$localpath = "C:\latest" 
+$sourcedir = Invoke-WebRequest https://packages.vmware.com/tools/esx/latest/windows/x64/
+
+$fileurl = $sourcedir.BaseResponse.ResponseUri.ToString() + $sourcedir.Links[1].href
+$fullpath = $localpath +"\"+ $sourcedir.Links[1].href
+
+if (test-path $fullpath){
+    Write-Output "file already exisit"
+} else {
+    Start-BitsTransfer -Source $fileurl -Destination $fullpath
+}
